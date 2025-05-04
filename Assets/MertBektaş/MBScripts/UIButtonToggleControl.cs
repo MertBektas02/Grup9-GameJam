1a1316c32a049bc3bbbe5b64b8ed4ed5641eb50a
Emre Yapıcı <emreeyapicii@gmail.com>
2025-05-04 15:13:57 +0300

    Ship Screen Info!

--- Assets/MertBektaş/MBScripts/UIButtonToggleControl.cs
+++ Assets/MertBektaş/MBScripts/UIButtonToggleControl.cs
@@ -16,19 +16,15 @@ public class UIButtonTriggerControl : MonoBehaviour
 
     void Start()
     {
-        if (buttonObject != null)
-            buttonObject.SetActive(false); // Buton başta gizli
-
-        if (cameraTransform == null && Camera.main != null)
-            cameraTransform = Camera.main.transform;
+        panelObject.SetActive(false);
     }
 
     void Update()
     {
-        RotateButtonToFaceCamera();
-        RotatePanelToFaceCamera();
+        // RotateButtonToFaceCamera();
+        // RotatePanelToFaceCamera();
         CheckToggleInput();
-        LookAtCamera();
+        // LookAtCamera();
     }
 
     // Butona P tuşu ile tıklama simülasyonu
@@ -51,45 +47,45 @@ public class UIButtonTriggerControl : MonoBehaviour
     }
 
     // Buton kameraya dönük kalsın
-    private void RotateButtonToFaceCamera()
-    {
-        if (buttonObject != null && buttonObject.activeSelf && cameraTransform != null)
-        {
-            buttonObject.transform.LookAt(cameraTransform);
-            buttonObject.transform.rotation = Quaternion.Euler(0, buttonObject.transform.eulerAngles.y + 180f, 0);
-        }
-    }
+    //private void RotateButtonToFaceCamera()
+    //{
+    //    if (buttonObject != null && buttonObject.activeSelf && cameraTransform != null)
+    //    {
+    //        buttonObject.transform.LookAt(cameraTransform);
+    //        buttonObject.transform.rotation = Quaternion.Euler(0, buttonObject.transform.eulerAngles.y + 180f, 0);
+    //    }
+    //}
 
     //panel kameraya baksın
-    private void RotatePanelToFaceCamera()
-{
-    if (panelObject != null && panelObject.activeSelf && cameraTransform != null)
-    {
-        panelObject.transform.LookAt(cameraTransform);
-        panelObject.transform.rotation = Quaternion.Euler(0, panelObject.transform.eulerAngles.y + 180f, 0);
-    }
-}
+//    private void RotatePanelToFaceCamera()
+//{
+//    if (panelObject != null && panelObject.activeSelf && cameraTransform != null)
+//    {
+//        panelObject.transform.LookAt(cameraTransform);
+//        panelObject.transform.rotation = Quaternion.Euler(0, panelObject.transform.eulerAngles.y + 180f, 0);
+//    }
+//}
 
     // Panelin pozisyonunu kameraya göre güncelle
-    private void LookAtCamera()
-    {
-        if (panelObject != null && panelObject.activeSelf && cameraTransform != null)
-        {
-            Vector3 forwardOffset = cameraTransform.forward * panelOffset.z;
-            Vector3 rightOffset = cameraTransform.right * panelOffset.x;
-            Vector3 upOffset = cameraTransform.up * panelOffset.y;
+    //private void LookAtCamera()
+    //{
+    //    if (panelObject != null && panelObject.activeSelf && cameraTransform != null)
+    //    {
+    //        Vector3 forwardOffset = cameraTransform.forward * panelOffset.z;
+    //        Vector3 rightOffset = cameraTransform.right * panelOffset.x;
+    //        Vector3 upOffset = cameraTransform.up * panelOffset.y;
 
-            panelObject.transform.position = cameraTransform.position + forwardOffset + rightOffset + upOffset;
-            panelObject.transform.rotation = Quaternion.LookRotation(panelObject.transform.position - cameraTransform.position);
-        }
-    }
+    //        panelObject.transform.position = cameraTransform.position + forwardOffset + rightOffset + upOffset;
+    //        panelObject.transform.rotation = Quaternion.LookRotation(panelObject.transform.position - cameraTransform.position);
+    //    }
+    //}
 
     // Trigger’a girince butonu göster
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ship"))
         {
-            buttonObject.SetActive(true);
+            panelObject.SetActive(true);
         }
     }
 
@@ -98,7 +94,7 @@ public class UIButtonTriggerControl : MonoBehaviour
     {
         if (other.CompareTag("Ship"))
         {
-            buttonObject.SetActive(false);
+            panelObject.SetActive(false);
         }
     }
 }
